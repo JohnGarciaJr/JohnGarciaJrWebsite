15189a1d66bcf0ceb6b050e18e042dd81eeafa63
John H Garcia Jr <johngarciajr@hotmail.com>
2023-04-11 15:07:50 -0500

    Update Save 7 (I think)

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ if(navClose){
     }
 }
 
-
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
@@ -33,7 +32,19 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== SWIPER PROJECTS ===============*/
-
+let swiperProjects = new Swiper(".projects__container", {
+    loop: true,
+    spaceBetween: 24,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
