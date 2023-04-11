c780ed95b5f2ad3400c03f02d5d4e93b1324cc35
John H Garcia Jr <johngarciajr@hotmail.com>
2023-04-11 16:40:01 -0500

    Update Save 10

--- main.js
+++ main.js
@@ -43,6 +43,12 @@ let swiperProjects = new Swiper(".projects__container", {
     pagination: {
       el: ".swiper-pagination",
     },
+    breakpoints: {
+        1200: {
+          slidesPerView: 2,
+          spaceBetween: -56,
+        },
+    },
 
   });
 
