165623d3100c511f4702f11d98a81b117102816d
John H Garcia Jr <johngarciajr@hotmail.com>
2023-04-11 19:04:11 -0500

    Update Save 13

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== SWIPER PROJECTS ===============*/
-let swiperProjects = new Swiper("projects__container", {
+let swiperProjects = new Swiper(".projects__container", {
     loop: true,
     spaceBetween: 24,
 
